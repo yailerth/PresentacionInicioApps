c69ab262bc8e488b8f21861086a5e2a807dcf212
yailerth <yailerth@gmail.com>
2020-09-22 21:50:58 -0500

    22 sept 2020 950pm

--- app/src/main/java/com/example/presentacioninicioapps/MainActivity.java
+++ app/src/main/java/com/example/presentacioninicioapps/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPagerPresent;
     private LinearLayout pointLayout;
+    private Button btnNext, btnBack;
+    private int currentPage;
 
     private TextView[] linearPoint;
 
@@ -24,12 +28,28 @@ public class MainActivity extends AppCompatActivity {
 
         viewPagerPresent = (ViewPager)findViewById(R.id.viewPagerPresent);
         pointLayout = (LinearLayout)findViewById(R.id.linearPoint);
+        btnBack = (Button)findViewById(R.id.btn_back);
+        btnNext = (Button)findViewById(R.id.btn_next);
 
         sliderAdapter = new SliderAdapter(this);
         viewPagerPresent.setAdapter(sliderAdapter);
 
         addLinearPoint(0);
         viewPagerPresent.addOnPageChangeListener(viewListener);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewPagerPresent.setCurrentItem(currentPage + 1);
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewPagerPresent.setCurrentItem(currentPage - 1);
+            }
+        });
 
     }
 
@@ -60,6 +80,31 @@ public class MainActivity extends AppCompatActivity {
         public void onPageSelected(int i) {
 
             addLinearPoint(i);
+            currentPage = i;
+
+            if (i<=0){
+                btnBack.setEnabled(false);
+                btnNext.setEnabled(true);
+                btnBack.setVisibility(View.INVISIBLE);
+
+                btnNext.setText("Next");
+                btnBack.setText("");
+
+            }else if (i == linearPoint.length-1){
+                btnBack.setEnabled(true);
+                btnNext.setEnabled(true);
+                btnBack.setVisibility(View.VISIBLE);
+
+                btnNext.setText("Finish");
+                btnBack.setText("Back");
+            }else{
+                btnBack.setEnabled(true);
+                btnNext.setEnabled(true);
+                btnBack.setVisibility(View.VISIBLE);
+
+                btnNext.setText("Next");
+                btnBack.setText("Back");
+            }
 
         }
 
